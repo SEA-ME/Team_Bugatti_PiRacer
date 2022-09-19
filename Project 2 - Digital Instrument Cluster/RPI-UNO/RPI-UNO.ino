cad86c2515dd778777f89fcedfd240065fd05c01
MoonGyu-Jeong <newsm5403@kookmin.ac.kr>
2022-09-19 17:55:34 +0200

    RPI-UNO examples

---
+++ Project 2 - Digital Instrument Cluster/RPI-UNO/RPI-UNO.ino
@@ -0,0 +1,63 @@
+#include <SPI.h>
+#include "mcp_can.h"
+#define CAN_125KBPS  13
+const int spiCSPin = 10;
+const int ledPin = 2;
+boolean ledON = 1;
+
+MCP_CAN CAN(spiCSPin);
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(ledPin,OUTPUT);
+
+    while (CAN_OK != CAN.begin(CAN_125KBPS,MCP_8MHz))
+    {
+        Serial.println("CAN BUS Init Failed");
+        delay(100);
+    }
+    Serial.println("CAN BUS  Init OK!");
+}
+
+
+void loop()
+{
+    unsigned char len = 0;
+    unsigned char buf[8];
+
+    if(CAN_MSGAVAIL == CAN.checkReceive())
+    {
+        CAN.readMsgBuf(&len, buf);
+
+        unsigned long canId = CAN.getCanId();
+
+        Serial.println("-----------------------------");
+        Serial.print("Data from ID: 0x");
+        Serial.println(canId, HEX);
+
+        for(int i = 0; i<len; i++)
+        {
+            Serial.print(buf[i]);
+            Serial.print("\t");
+            if(ledON && i==0)
+            {
+
+                digitalWrite(ledPin, buf[i]);
+                ledON = 0;
+                delay(500);
+            }
+            else if((!(ledON)) && i==4)
+            {
+
+                digitalWrite(ledPin, buf[i]);
+                ledON = 1;
+            }
+        }
+        Serial.println();
+    }
+    else{
+      Serial.print(CAN_MSGAVAIL);
+      Serial.println(CAN.checkReceive());
+    }
+}
